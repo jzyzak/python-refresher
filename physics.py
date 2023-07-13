db37f428bc751c2692a9217a8d93f982bd1d9124
jzyzak <joshuazyzak@gmail.com>
2023-07-13 11:37:38 -0400

    added physics docstring/comments and fixed tests

--- physics.py
+++ physics.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+g = 9.81  # m/s^2
+water_density = 1000  # kg/m^3
+surface_pressure = 101325  # kPa
+
 
 def calculate_buoyancy(V, density_fluid):
     '''Calculates the buoyancy of an object with volume "V" in a given fluid with density "density_fluid"'''
@@ -8,19 +12,25 @@ def calculate_buoyancy(V, density_fluid):
     ):  # raises an error if the fluid density or object volume is inputted as a negative number
         raise ValueError("Volume and fluid density cannot be negative.")
     else:  # calculates and returns buoyancy
-        return density_fluid * V * 9.81
+        return density_fluid * V * g  # N
 
 
 def will_it_float(V, mass):
-    """Returns true if an object with a given volume "V" and mass will float in water and false if it will sink in water."""
+    """Returns true if an object with a given volume "V" and mass will float in water. false if it will sink in water, and None if it is neutrally buoyant."""
     if (
         V < 0 or mass < 0
     ):  # raises an error if the object volume or mass is inputted as a negative number
         raise ValueError("Volume and mass cannot be negative.")
     else:
         density_object = mass / V  # calculates the density of the object
-        if density_object > 1000:  # returns false if the object won't float in water
+        if (
+            density_object > water_density
+        ):  # returns false if the object won't float in water
             return False
+        elif (
+            density_object == water_density
+        ):  # returns None if the object is neutrally buoyant
+            return None
         else:  # returns true if the object will float in water
             return True
 
@@ -30,4 +40,4 @@ def calculate_pressure(depth):
     if depth < 0:  # raises an error if the depth is inputted as a negative number
         raise ValueError("Depth cannot be negative.")
     else:  # calculates and returns the pressure at the given depth
-        return 1000 * 9.81 * depth
+        return water_density * g * depth + surface_pressure
--- test_physics.py
+++ test_physics.py
@@ -4,18 +4,24 @@ import physics
 
 class TestPhysics(unittest.TestCase):
     def test_calculateBuoyancy(self):
-        self.assertEqual(physics.calculate_buoyancy(10.0, 10.0), 981)
-        self.assertNotEqual(physics.calculate_buoyancy(100.0, 10.0), 981)
-        self.assertRaises(ValueError, physics.calculate_buoyancy, -1.0, 10.0)
-        self.assertRaises(ValueError, physics.calculate_buoyancy, 10.0, -1.0)
+        self.assertEqual(physics.calculate_buoyancy(10, 10), 981)
+        self.assertNotEqual(physics.calculate_buoyancy(100, 10), 981)
+        self.assertRaises(ValueError, physics.calculate_buoyancy, -1, 10)
+        self.assertRaises(ValueError, physics.calculate_buoyancy, 10, -1)
 
     def test_willItFloat(self):
-        self.assertEqual(physics.will_it_float(1.0, 10000.0), False)
-        self.assertEqual(physics.will_it_float(10000.0, 1.0), True)
+        self.assertEqual(physics.will_it_float(1, 10000), False)
+        self.assertEqual(physics.will_it_float(10000, 1), True)
         self.assertRaises(ValueError, physics.will_it_float, -1.0, 10000.0)
         self.assertRaises(ValueError, physics.will_it_float, 10000.0, -1.0)
 
     def test_calculatePressure(self):
-        self.assertEqual(physics.calculate_pressure(1.0), 9810)
-        self.assertNotEqual(physics.calculate_pressure(100.0), 9810)
-        self.assertRaises(ValueError, physics.calculate_pressure, -1.0)
+        self.assertEqual(physics.calculate_pressure(0), 101325)
+        self.assertNotEqual(physics.calculate_pressure(0), 0)
+        self.assertRaises(ValueError, physics.calculate_pressure, -1)
+
+
+if __name__ == "__main__":
+    print(physics.calculateBuoyancy(10, 10))
+    print(physics.willItFloat(10000, 1), True)
+    print(physics.calculatePressure(1))
